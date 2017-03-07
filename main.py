3e68535c2123fee1c999ff713214629e1cb7722d
yungchunkan <yungchunkan@gmail.com>
2017-03-07 18:02:34 +0800

    add login view

--- main.py
+++ main.py
@@ -14,12 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+
 import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'www/templates')
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world! 31234')
+        template = JINJA_ENVIRONMENT.get_template('login.html')
+        self.response.write(template.render())
+
+    def post(self):
+        self.response.write("nothing")
 
 
 app = webapp2.WSGIApplication([
