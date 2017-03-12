ff5c5764e58a0078835d4b1ad802a6f0b50fa9d8
yungchunkan <yungchunkan@gmail.com>
2017-03-12 22:01:46 +0800

    add product add page

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ import os
 import webapp2
 import jinja2
 
+from entities.Brand import Brand
+from entities.Category import Category
 from entities.Order import Order
 from entities.Account import Account
 from google.appengine.api import users
+
+from entities.Product import Product
 
 template_dir = os.path.join(os.path.dirname(__file__), 'www/templates')
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -35,9 +39,9 @@ class MainHandler(webapp2.RequestHandler):
         user = users.get_current_user()
         if user:
             Account.get_or_insert('id', id=user.user_id(), name=user.nickname())
-            auth_link = users.create_logout_url("/")
+            auth_link = users.create_logout_url(self.request.url)
         else:
-            auth_link = users.create_login_url("/")
+            auth_link = users.create_login_url(self.request.url)
 
         template_var = {
             "auth_link": auth_link
@@ -54,10 +58,10 @@ class OrderAddHandler(webapp2.RequestHandler):
     def post(self):
         user = users.get_current_user()
         name = self.request.get("name")
-        desc = self.request.get("description")
-        user = Account.get_by_google_id(user.user_id())
-        # order = Order(name=name, description=desc, user=user)
-        # order.put()
+        desc = self.request.get("description", "")
+        user = Account.get_or_insert('id', id=user.user_id(), name=user.nickname())
+        order = Order(name=name, description=desc, user=user)
+        order.put()
         self.redirect("/order/add")
 
 
@@ -67,8 +71,24 @@ class OrderHandler(webapp2.RequestHandler):
         self.redirect("/")
 
 
+class ProductAddHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('product_add.html')
+        self.response.write(template.render())
+
+    def post(self):
+        name = self.request.get("name")
+        desc = self.request.get("description")
+        category = Category.get_by_id(self.request.get("category", 0))
+        brand = Brand.get_by_id(self.request.get("brand", 0))
+        product = Product(name=name, description=desc, category=category, brand=brand)
+        product.put()
+        self.redirect("/order/add")
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/order/add', OrderAddHandler),
-    ('/order/(\d+)', OrderHandler)
+    ('/order/(\d+)', OrderHandler),
+    ('/order/product/add', ProductAddHandler)
 ], debug=True)
