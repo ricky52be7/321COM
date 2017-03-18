ad64ce998f8ba3d57ce7077aace9baac2710cc48
KNM1993 <kevin199399@hotmail.com>
2017-03-18 16:26:43 +0800

    Modified: Order.py name add lowercase var. added method for search bar

--- entities/Order.py
+++ entities/Order.py
@@ -16,9 +16,27 @@ class Order(ndb.Model):
               STATUS_PENDING: "pending"}
 
     name = ndb.StringProperty(required=True)
+    name_lower = ndb.ComputedProperty(lambda self: self.name.lower())
     description = ndb.TextProperty()
     user = ndb.StructuredProperty(Account, required=True)
     product_ids = ndb.IntegerProperty(repeated=True)
     create_at = ndb.DateTimeProperty(auto_now_add=True)
     update_at = ndb.DateTimeProperty(auto_now=True)
     status = ndb.IntegerProperty(default=STATUS_PENDING)
+
+    @classmethod
+    def sort_status(cls):
+        return cls.query(cls.status == 1).order(-cls.update_at)
+
+    @classmethod
+    def search_name(cls, name_key):
+        return cls.query(cls.name == name_key, cls.status == 1).order(-cls.update_at)
+
+    @classmethod
+    def search_name_lc(cls, name_key):
+        name_key = name_key.lower()
+        limit = name_key[:-1] + chr(ord(name_key[-1]) +1)
+        return cls.query(Order.name_lower >= name_key, Order.name_lower < limit).fetch(50)
+
+
+
